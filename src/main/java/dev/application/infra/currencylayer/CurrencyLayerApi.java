cccf925ae1dfcb1ec08db1676783763df16cd9aa
JanghoPark <pjhsk113@naver.com>
2021-11-18 00:04:08 +0900

    feat: [캐싱 기능] 캐시 비우기 스케줄링 기능 추가
    - 캐싱 만료 기준 시간을 사용자의 요청 시점으로 잡으면 부정확한 데이터가 전달될 수 있다.
    따라서 정확한 환율 정보 제공을 위해 주기적으로 캐시를 비워주는 스케줄링(30분 간격) 기능을 추가함

--- src/main/java/dev/application/infra/currencylayer/CurrencyLayerApi.java
+++ src/main/java/dev/application/infra/currencylayer/CurrencyLayerApi.java
@@ -1,25 +1,38 @@
 package dev.application.infra.currencylayer;
 
 import dev.application.infra.currencylayer.dto.CurrencyLayerApiResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
+
 @Component
+@RequiredArgsConstructor
 public class CurrencyLayerApi {
     private static final String URI = "/live?access_key=";
 
+    @Value("${api.accessKey}")
     private final String accessKey;
     private final RestTemplate currencyLayerTemplate;
-
-    public CurrencyLayerApi(@Value("${api.accessKey}") String accessKey, RestTemplate currencyLayerTemplate) {
-        this.accessKey = accessKey;
-        this.currencyLayerTemplate = currencyLayerTemplate;
-    }
+    private final CacheManager cacheManager;
 
     @Cacheable(cacheNames = "exchange_rate")
     public CurrencyLayerApiResponse getExchangeRate() {
         return currencyLayerTemplate.getForObject(URI + accessKey, CurrencyLayerApiResponse.class);
+    }
+
+    // 매일 8:00시 ~ 18:00시 동안 30분 간격으로 캐시 비우기
+    @Scheduled(cron = "0 0/30 8-18 * * *")
+    public void refreshCache() {
+        for (String name : cacheManager.getCacheNames()) {
+            System.out.printf("[%s] %s%n", java.time.LocalTime.now().truncatedTo(ChronoUnit.MILLIS), name);
+            Objects.requireNonNull(cacheManager.getCache(name)).clear();
+        }
     }
 }
