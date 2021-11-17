c75f118e03f9cf5b7c5d7e26efc57aedf3a6182a
JanghoPark <pjhsk113@naver.com>
2021-11-17 19:25:07 +0900

    feat: [환율 계산 개발] 수췩 금액 계산을 담당하는 API 추가

--- src/main/java/dev/application/domain/exchangerate/api/ExchangeRateQueryController.java
+++ src/main/java/dev/application/domain/exchangerate/api/ExchangeRateQueryController.java
@@ -2,12 +2,17 @@ package dev.application.domain.exchangerate.api;
 
 import dev.application.domain.exchangerate.application.CurrencyInformationManager;
 import dev.application.domain.exchangerate.domain.ExchangeRate;
+import dev.application.domain.exchangerate.dto.request.ExchangeAmountRequest;
+import dev.application.domain.exchangerate.dto.response.ExchangeAmountResponse;
 import dev.application.domain.exchangerate.dto.response.ExchangeRateResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.math.BigDecimal;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,4 +26,9 @@ public class ExchangeRateQueryController {
         return ResponseEntity.ok(ExchangeRateResponse.of(exchangeRate));
     }
 
+    @GetMapping("/amount")
+    public ResponseEntity<ExchangeAmountResponse> getExchangeAmount(@Valid ExchangeAmountRequest request) {
+        BigDecimal exchangeAmount = currencyInformationManager.calculateAmount(request);
+        return ResponseEntity.ok(ExchangeAmountResponse.of(exchangeAmount));
+    }
 }
--- src/main/java/dev/application/domain/exchangerate/application/CurrencyInformationManager.java
+++ src/main/java/dev/application/domain/exchangerate/application/CurrencyInformationManager.java
@@ -1,10 +1,13 @@
 package dev.application.domain.exchangerate.application;
 
 import dev.application.domain.exchangerate.domain.ExchangeRate;
+import dev.application.domain.exchangerate.dto.request.ExchangeAmountRequest;
 import dev.application.infra.currencylayer.CurrencyLayerApi;
 import dev.application.infra.currencylayer.dto.CurrencyLayerApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 @RequiredArgsConstructor
@@ -17,4 +20,8 @@ public class CurrencyInformationManager {
         return CurrencyLayerApiResponse.newExchangeRate(response);
     }
 
+    public BigDecimal calculateAmount(ExchangeAmountRequest request) {
+        ExchangeRate exchangeRate = getSupportedExchangeRate();
+        return CurrencyOperator.calculate(request, exchangeRate);
+    }
 }
