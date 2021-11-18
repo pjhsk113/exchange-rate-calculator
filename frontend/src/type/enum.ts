759ee14bc6e5dfca53c2f8ce87d3191d683b0279
JanghoPark <pjhsk113@naver.com>
2021-11-19 00:21:23 +0900

    feat: [프론트엔드] 화면에서 사용할 enum 정의

---
+++ frontend/src/type/enum.ts
@@ -0,0 +1,12 @@
+
+export enum CurrencyExchangeCodes {
+  한국 = 'usdkrw',
+  일본 = 'usdjpy',
+  필리핀 = 'usdphp'
+}
+
+export enum CurrencyCodes {
+  usdkrw = 'KRW',
+  usdjpy = 'JPY',
+  usdphp = 'PHP',
+}
