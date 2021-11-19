0a0a63946b860e1ed2f1b6747d1c3920c60ab392
JanghoPark <pjhsk113@naver.com>
2021-11-19 19:01:45 +0900

    refactor: Controller 이름 변경
    - Receiver로 변경하여 의미가 더 잘 드러나도록 변경

--- src/main/java/dev/application/domain/exchangerate/api/ExchangeRateQueryReceiver.java
+++ src/main/java/dev/application/domain/exchangerate/api/ExchangeRateQueryReceiver.java
@@ -1,7 +1,6 @@
 package dev.application.domain.exchangerate.api;
 
 import dev.application.domain.exchangerate.application.CurrencyInformationManager;
-import dev.application.domain.exchangerate.domain.ExchangeRate;
 import dev.application.domain.exchangerate.dto.request.ExchangeAmountRequest;
 import dev.application.domain.exchangerate.dto.response.ExchangeAmountResponse;
 import dev.application.domain.exchangerate.dto.response.ExchangeRateResponse;
@@ -12,23 +11,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.math.BigDecimal;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/exchange-rate")
-public class ExchangeRateQueryController {
+public class ExchangeRateQueryReceiver {
     private final CurrencyInformationManager currencyInformationManager;
 
     @GetMapping("/info")
     public ResponseEntity<ExchangeRateResponse> getExchangeRateInformation() {
-        ExchangeRate exchangeRate = currencyInformationManager.getSupportedExchangeRate();
-        return ResponseEntity.ok(ExchangeRateResponse.of(exchangeRate));
+        return ResponseEntity.ok(
+                ExchangeRateResponse.of(currencyInformationManager.getSupportedExchangeRate())
+        );
     }
 
     @GetMapping("/amount")
     public ResponseEntity<ExchangeAmountResponse> getExchangeAmount(@Valid ExchangeAmountRequest request) {
-        BigDecimal exchangeAmount = currencyInformationManager.calculateAmount(request);
-        return ResponseEntity.ok(ExchangeAmountResponse.of(exchangeAmount));
+        return ResponseEntity.ok(
+                ExchangeAmountResponse.of(currencyInformationManager.calculateAmount(request))
+        );
     }
 }
