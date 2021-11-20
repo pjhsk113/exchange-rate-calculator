39e4fe5adc3979c009ac31d238eb239d4665e646
JanghoPark <pjhsk113@naver.com>
2021-11-20 13:30:06 +0900

    refactor: Error Code 변경

--- src/main/java/dev/application/global/error/exception/ErrorCode.java
+++ src/main/java/dev/application/global/error/exception/ErrorCode.java
@@ -5,22 +5,22 @@ public enum ErrorCode {
     /**
      * HTTP Method 오류 (405)
      */
-    METHOD_NOT_ALLOWED(405, "C001", "지원하지 않는 HTTP 메소드입니다. 요청 방식을 확인해주세요."),
+    METHOD_NOT_ALLOWED(405, "E3001", "지원하지 않는 HTTP 메소드입니다. 요청 방식을 확인해주세요."),
 
     /**
      * 요청 입력 값 오류 (400)
      */
-    INVALID_INPUT_VALUE(400, "C002", "입력 값이 올바르지 않습니다."),
+    INVALID_INPUT_VALUE(400, "E3002", "입력 값이 올바르지 않습니다."),
 
     /**
      * 외부 API 조회 오류
      */
-    UNABLE_EXTERNAL_API_CALL_ERROR(406, "C003", "요청에 성공했지만, 외부 API 콘텐츠 특성으로 응답할 수 없습니다."),
+    UNABLE_EXTERNAL_API_CALL_ERROR(406, "E3003", "요청에 성공했지만, 외부 API 콘텐츠 특성으로 응답할 수 없습니다."),
 
     /**
      * 서버 내부 오류 (500)
      */
-    INTERNAL_SERVER_ERROR(500, "C000", "서버에 알 수 없는 오류가 발생했습니다.");
+    INTERNAL_SERVER_ERROR(500, "E3999", "서버에 알 수 없는 오류가 발생했습니다.");
 
     private final String code;
     private final String message;
